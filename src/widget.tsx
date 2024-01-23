6d1230fd7b7b39cfd614c9957a308a9ef76cda68
Stijn E <stijn.elskens@riskconcile.com>
2024-01-23 22:31:25 +0100

    wip

--- src/widget.tsx
+++ src/widget.tsx
@@ -1,23 +1,34 @@
 import {DashboardWidgetContainer} from '@sanity/dashboard'
 
 export interface WidgetConfig {
-  // eslint-disable-next-line react/no-unused-prop-types, react/require-default-props
   title?: string
+  url: string
+  height?: string
 }
 
 const defaultProps = {
-  title: 'Last created',
+  title: 'Plausible Analytics',
 }
 
 export function Widget(props: WidgetConfig) {
-  const {title} = {
-    ...defaultProps,
-    ...props,
-  }
+  const {title, url, height} = { ...defaultProps, ...props}
 
   return (
     <DashboardWidgetContainer header={title}>
-      <div>Helllooo, testing</div>
+      <>
+        <iframe
+          src={`${url}&embed=true&theme=system`}
+          loading="lazy"
+          style={{
+            width: '100%',
+            minWidth: 'calc(100% - 1px)',
+            height: height ? height : 'calc(100vh - 143px)',
+            border: 'none',
+            verticalAlign: 'middle',
+          }}
+        />
+        <script async src="https://plausible.io/js/embed.host.js" />
+      </>
     </DashboardWidgetContainer>
   )
 }
